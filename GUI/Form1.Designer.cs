f80e5bd08210c36586d1b44d77f8a564b3b8c568
omarosamasobeih96 <omarosamasobeih@yahoo.com>
2019-06-10 23:50:55 +0200

    first commit

---
+++ GUI/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace GUI {
+	partial class Form1 {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.select_video = new System.Windows.Forms.Button();
+			this.classification = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(12, 51);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(848, 546);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// select_video
+			// 
+			this.select_video.Font = new System.Drawing.Font("Segoe UI", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.select_video.Location = new System.Drawing.Point(940, 88);
+			this.select_video.Name = "select_video";
+			this.select_video.Size = new System.Drawing.Size(191, 87);
+			this.select_video.TabIndex = 1;
+			this.select_video.Text = "Select Video";
+			this.select_video.UseVisualStyleBackColor = true;
+			this.select_video.Click += new System.EventHandler(this.select_video_Click);
+			// 
+			// classification
+			// 
+			this.classification.AutoSize = true;
+			this.classification.Font = new System.Drawing.Font("Segoe UI", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.classification.Location = new System.Drawing.Point(648, 104);
+			this.classification.Name = "classification";
+			this.classification.Size = new System.Drawing.Size(192, 38);
+			this.classification.TabIndex = 2;
+			this.classification.Text = "classification";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1223, 687);
+			this.Controls.Add(this.classification);
+			this.Controls.Add(this.select_video);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button select_video;
+		private System.Windows.Forms.Label classification;
+	}
+}
+
---
+++ GUI/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace GUI {
+	public partial class Form1 : Form {
+
+		private VideoCapture capture = null;
+		private bool is_playing = false;
+		private int frame_count;
+		private int running_frame_index;
+		private int frame_rate;
+		private Mat running_frame;
+		private Mat frame;
+		private double[] predictions;
+		private int segment_size;
+		private int running_segment_index;
+
+		private void ProcessFrame(object sender, EventArgs e) {
+			if (capture != null && capture.Ptr != IntPtr.Zero) {
+				capture.Retrieve(frame, 0);
+				pictureBox1.Image = frame.Bitmap;
+			}
+		}
+
+		private async void play_video() {
+			if (capture == null) return;
+			capture.ImageGrabbed += ProcessFrame;
+			frame = new Mat();
+			try {
+				while (is_playing == true && running_frame_index < frame_count) {
+					if (running_frame_index == segment_size * (1 + running_segment_index)) running_segment_index += 1;
+					capture.SetCaptureProperty(Emgu.CV.CvEnum.CapProp.PosFrames, running_frame_index);
+					capture.Read(running_frame);
+					pictureBox1.Image = running_frame.Bitmap;
+					running_frame_index += 1;
+					classification.Text = predictions[running_segment_index].ToString();
+					await Task.Delay(500 / frame_rate);
+				}
+			}
+			catch (Exception ex) {
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+		private String get_prediction_filename(String video_filename) {
+			String prediction_filename = "";
+			int len = video_filename.Length;
+			for(int i = len - 1; i >= 0; --i) {
+				if(video_filename[i] == '.') {
+					for (int j = 0; j < i; ++j)
+						prediction_filename += video_filename[j];
+					break;
+				}
+			}
+			return prediction_filename + ".mat";
+		}
+
+		public Form1() {
+			InitializeComponent();
+		}
+
+		private void select_video_Click(object sender, EventArgs e) {
+			is_playing = false;
+			OpenFileDialog open_file_dialogue = new OpenFileDialog();
+			open_file_dialogue.Filter = "Video Files (*.mp4, *.flv)| *.mp4;*.flv";
+			if (open_file_dialogue.ShowDialog() == DialogResult.OK) {
+				String prediction_filename = get_prediction_filename(open_file_dialogue.FileName);
+				String[] reader = System.IO.File.ReadAllLines(prediction_filename);
+				predictions = new double[32];
+				for (int i = 0; i < 32; ++i) predictions[i] = Convert.ToDouble(reader[i]);
+				capture = new VideoCapture(open_file_dialogue.FileName);
+				frame_count = Convert.ToInt32(capture.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.FrameCount));
+				segment_size = (frame_count + 31) / 32;
+				frame_rate = Convert.ToInt32(capture.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.Fps));
+				is_playing = true;
+				running_frame = new Mat();
+				running_frame_index = 0;
+				running_segment_index = 0;
+				play_video();
+			}
+		}
+	}
+}
